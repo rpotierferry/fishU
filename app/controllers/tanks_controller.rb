c090925ed0bb782f42cbb0a57cecb4046bc76d08
Mylene Leblanc <leblanc.pommier.mylene@gmail.com>
2024-03-05 14:56:09 +0100

    add methode controller

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,5 +1,6 @@
 class TanksController < ApplicationController
-  before_action :set_tank, only: %i[show feed]
+  before_action :set_tank_and_user, only: %i[show feed bubble]
+
   def index
     @tanks = Tank.all
   end
@@ -21,15 +22,51 @@ class TanksController < ApplicationController
   def feed
     @tank.fish.each do |f|
     f.size += 1
+    f.fed = true
     f.save
     @tank.nitrate += 1
     @tank.save
     end
   end
 
+  def bubble(q)
+    @user.currency += q
+    @user.save
+  end
+
+  def add_plant
+    @plant = Plant.new(@tank)
+    @plant.save
+  end
+
+  def plant_action
+    @tank.nitrate -= @tank.plant.coun
+    @tank.save
+  end
+
+  def add_lamp
+    @tank.has_lamp = true
+    @tank.plant.each do |p|
+    p.life_expectancy = nil
+    p.save
+    end
+    @tank.save
+  end
+
+  def increase_tank_size
+    @tank.liters += 5
+    @tank.save
+  end
+
+  def new_day
+    plant_action
+  end
+
   private
 
-  def set_tank
+  def set_tank_and_user
     @tank = Tank.find(params[:id])
+    @user = @tank.user
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_101258) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_135445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_101258) do
     t.integer "size", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "fed", default: false
     t.index ["tank_id"], name: "index_fish_on_tank_id"
   end
 
