a0cd6e79da1341cb6647398a63d4b3e89d784c5a
Romain Potier Ferry <romainpotierferry@gmail.com>
2024-03-07 16:30:18 +0100

    tank defaults

--- db/migrate/20240305100841_change_default_values_in_tanks.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDefaultValuesInTanks < ActiveRecord::Migration[7.1]
-  def change
-    change_column_default :tanks, :liters, 5
-    change_column_default :tanks, :nitrate, 0
-    change_column_default :tanks, :has_lamp, false
-  end
-end
