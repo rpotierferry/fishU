aa66c192542f6547eee5362162b148fc803ff087
Mylene Leblanc <leblanc.pommier.mylene@gmail.com>
2024-03-13 15:22:05 +0100

    Js pour l'animatio nuit et pop up

---
+++ app/javascript/controllers/night_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="night"
+export default class extends Controller {
+  connect() {
+    console.log("salut les copains ");
+  }
+
+  nightAnimation(event) {
+    event.preventDefault()
+    const form = event.currentTarget.closest(".button_to")
+    document.querySelector(".day").classList.add("to_night")
+    setTimeout(() => {
+      form.submit()
+    }, 1000);
+  }
+
+  notFed(event) {
+    event.preventDefault()
+    const popup = `
+    <div class="alert">
+      Vous ne pouvez pas ...
+    </div>
+    `
+    document.body.insertAdjacentHTML("beforeend", popup)
+  }
+}
